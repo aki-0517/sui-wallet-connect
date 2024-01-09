c95354a9bd1d914b84f2df3fea6a00dbc2aec474
aki-0517 <akki.pp.777@gmail.com>
2024-01-09 21:28:12 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { ConnectButton, useCurrentAccount } from '@mysten/dapp-kit';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ConnectButton />
+            </header>
+
+            <ConnectedAccount />
+        </div>
+    );
+}
+
+function ConnectedAccount() {
+    const account = useCurrentAccount();
+
+    if (!account) {
+        return null;
+    }
+
+    return <div>Connected to {account.address}</div>;
+}
+
+export default App
